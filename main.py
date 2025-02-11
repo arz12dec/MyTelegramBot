6d565d4681dd593818a14dd735b0dce9aa3b0e32
arz12dec <arz12dec@gmail.com>
2025-02-12 03:01:23 +0330

    Update main.py

--- main.py
+++ main.py
@@ -57,4 +57,4 @@ def auto_reply(message):
         bot.reply_to(message, auto_replies[text], parse_mode="Markdown")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)),debug=True)
